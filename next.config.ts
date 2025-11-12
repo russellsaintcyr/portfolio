c2e1ee6ecb7e7b7948585eadca77dc0e8b4f7905
1111x <russellsaintcyr@gmail.com>
2025-11-11 20:16:59 -0500

    Configured distDir to use docs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: 'docs',
   // Optional: If your repo name is 'my-app', you need a basePath for correct asset paths
   // basePath: '/my-app',
 };
