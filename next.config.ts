67f659b9b14ac2b62b3a4e10a4d74593ecc44f97
1111x <russellsaintcyr@gmail.com>
2025-11-11 20:29:01 -0500

    Backed out distDir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'docs',
   // Optional: If your repo name is 'my-app', you need a basePath for correct asset paths
   // basePath: '/my-app',
 };
