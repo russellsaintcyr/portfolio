a7c796bee21fa294ed132e5662e0c7b4e123410d
1111x <russellsaintcyr@gmail.com>
2025-11-24 22:19:46 -0500

    Configured for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
-// Image Optimization using the default loader is not compatible with `{ output: 'export' }`.
+const isGitHubPages = process.env.GITHUB_ACTIONS || process.env.EXPORT_MODE;
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  images: { 
-    unoptimized: true 
-  }
+  ...(isGitHubPages && {
+    output: 'export',
+    basePath: '/portfolio',
+    images: { 
+      unoptimized: true 
+    }
+  }),
+  ...(!isGitHubPages && {
+    images: {
+      domains: [], // Add any external image domains here if needed
+    }
+  })
 };
 
 export default nextConfig;
