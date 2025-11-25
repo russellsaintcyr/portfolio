62c3afd992c2a3dc5f3dd52f40e869f534b49e47
1111x <russellsaintcyr@gmail.com>
2025-11-24 22:37:15 -0500

    Added next.config changes

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,25 @@ import type { NextConfig } from "next";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS || process.env.EXPORT_MODE;
 
+
 const nextConfig: NextConfig = {
   ...(isGitHubPages && {
     output: 'export',
     basePath: '/portfolio',
     images: { 
       unoptimized: true 
-    }
+    },
+    env: {
+      NEXT_PUBLIC_BASE_PATH: '/portfolio',
+    },
   }),
   ...(!isGitHubPages && {
     images: {
       domains: [], // Add any external image domains here if needed
-    }
+    },
+    env: {
+      NEXT_PUBLIC_BASE_PATH: '',
+    },
   })
 };
 
