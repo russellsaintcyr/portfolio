36882b76e53cb115053e9c17fafffd1bfe3988af
1111x <russellsaintcyr@gmail.com>
2025-12-26 20:10:42 -0500

    Fixed lyrics

--- app/components/LyricsCarousel.tsx
+++ app/components/LyricsCarousel.tsx
@@ -26,39 +26,42 @@ export default function LyricsCarousel({ lyrics }: LyricsCarouselProps) {
       <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
         Special Lyrics
       </h2>
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 10000,
-          disableOnInteraction: false,
-        }}
-        className="pb-12"
-      >
-        {lyrics.map((lyric, idx) => (
-          <SwiperSlide key={idx}>
-            <div className="h-full p-6 bg-white dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-700 shadow-sm flex flex-col items-center justify-center text-center">
-              <div
-                className="text-gray-700 dark:text-gray-300 whitespace-pre-line italic mb-4"
-                dangerouslySetInnerHTML={{ __html: lyric.text }}
-              />
-              {lyric.artist && (
-                <div className="text-gray-600 dark:text-gray-400">
-                  by {lyric.artist}
-                </div>
-              )}
-              {lyric.song && (
-                <div className="text-gray-600 dark:text-gray-400">
-                  from {lyric.song}
-                </div>
-              )}
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={lyrics.length > 1}
+          navigation={{
+            enabled: true,
+          }}
+          pagination={{ 
+            clickable: true,
+            dynamicBullets: false,
+          }}
+          autoplay={{
+            delay: 10000,
+            disableOnInteraction: false,
+          }}
+          className="lyrics-carousel"
+        >
+          {lyrics.map((lyric, idx) => (
+            <SwiperSlide key={idx}>
+              <div className="h-[320px] md:h-[250px] p-6 pb-12 bg-white dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-700 shadow-sm flex flex-col items-center justify-center text-center relative overflow-y-auto">
+                <div
+                  className="text-gray-700 dark:text-gray-300 whitespace-pre-line italic mb-4"
+                  dangerouslySetInnerHTML={{ __html: lyric.text }}
+                />
+                {lyric.artist && lyric.song && (
+                  <div className="text-gray-600 dark:text-gray-400">
+                    by {lyric.artist}, from {lyric.song}
+                  </div>
+                )}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
